e23eb3782bd89847eb0a068d616758bc3e649798
Abel Wang <abel.wang@microsoft.com>
2020-07-29 17:22:13 -0700

    created action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,45 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // get input values
+    console.log("In action, getting github token");
+    const myToken = core.getInput('githubToken');
+    console.log("myToken: " + myToken);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // get reference to octokit
+    const octokit = github.getOctokit(myToken);
 
-    core.setOutput('time', new Date().toTimeString())
+    // get reference to current context and get info about pull request
+    const context = github.context;
+    const prUrl = context.payload.pull_request?.html_url;
+    const prBody = context.payload.pull_request?.body;
+    const prNumber: number = <number>context.payload.pull_request?.number;
+    const { data: pullRequest } = await octokit.pulls.get( {
+      owner: 'abelsquidhead',
+      repo: 'TailwindsTraders-Website',
+      pull_number: prNumber,
+ 
+    });
+    const title = pullRequest.title
 
-    console.log("done!");
+    // print out the info from PR
+    console.log("prUrl: " + prUrl);
+    console.log("title: " + title);
+    console.log("number: "+ prNumber);
+
+    // save values as environment variables
+    core.exportVariable("PR_URL", prUrl);
+    core.exportVariable("PR_TITLE", title);
+    core.exportVariable("PR_NUMBER", prNumber);
+
+    // output values as output parameters
+    core.setOutput("url", prUrl);
+    core.setOutput("title", title);
+    core.setOutput("number", prNumber);
   } catch (error) {
     core.setFailed(error.message)
   }
